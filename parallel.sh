5541cf1255a5717dccb68df44b6bd14d135b942d
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-10-11 08:06:36 +0900

    support logs

--- parallel.sh
+++ parallel.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-declare -A RUNNAMES 
+declare -A RUNNAMES
 for i in $(seq 1 3); do
-  RUNNAMES[$(docker-compose run -d ok)]=1
+  sleep 1
+  RUNNED=$(docker-compose run -d ok)
+  echo $RUNNED started
+  docker logs -f -t $RUNNED &
+  RUNNAMES[$RUNNED]=1
 done
 
 while [ ${#RUNNAMES[@]} -gt 0 ]; do
   for NAME in ${!RUNNAMES[@]}; do
-    if ! docker-compose ps | grep $NAME ; then
+    if ! docker ps | grep -q $NAME ; then
       echo $NAME ended
       RET=$(docker inspect $NAME --format='{{.State.ExitCode}}')
       if [ "$RET" -ne 0 ]; then
-        echo restarted
-        RUNNAMES[$(docker-compose run -d ok)]=1 
+        RUNNED=$(docker-compose run -d ok)
+        docker logs -f -t $RUNNED &
+        RUNNAMES[$RUNNED]=1
+        echo $NAME restarted as $RUNNED
       fi
       unset -v RUNNAMES[$NAME]
     fi
